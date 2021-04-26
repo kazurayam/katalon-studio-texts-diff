d63fb41b41e54a1a8f6d44bb5a865262f08143f3
kazurayam <kazuaki.urayama@gmail.com>
2021-04-26 12:01:04 +0900

    report number of tags

--- Scripts/TC1/Script1619137698459.groovy
+++ Scripts/TC1/Script1619137698459.groovy
@@ -2,6 +2,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.function.Function
+import java.util.stream.Collectors
 
 import com.github.difflib.text.DiffRow
 import com.github.difflib.text.DiffRowGenerator
@@ -27,15 +28,30 @@ DiffRowGenerator generator =
 
 List<DiffRow> rows = generator.generateDiffRows(original, revised)
 
+List<DiffRow> insertedRows = rows.stream().filter({ DiffRow dr -> dr.getTag() == DiffRow.Tag.INSERT}).collect(Collectors.toList())
+List<DiffRow> deletedRows  = rows.stream().filter({ DiffRow dr -> dr.getTag() == DiffRow.Tag.DELETE}).collect(Collectors.toList())
+List<DiffRow> changedRows  = rows.stream().filter({ DiffRow dr -> dr.getTag() == DiffRow.Tag.CHANGE}).collect(Collectors.toList())
+List<DiffRow> equalRows    = rows.stream().filter({ DiffRow dr -> dr.getTag() == DiffRow.Tag.EQUAL}).collect(Collectors.toList())
+
 // print the diff info in Markdown format into a file out.md
 StringBuilder sb = new StringBuilder()
 sb.append("- original: `${doc1}`\n")
 sb.append("- revised : `${doc2}`\n\n")
+
+sb.append((equalRows.size() < rows.size()) ? '**DIFFERENT**' : '**NO DIFF**')
+sb.append("\n\n")
+
+sb.append("- inserted rows: ${insertedRows.size()}\n")
+sb.append("- deleted rows : ${deletedRows.size()}\n")
+sb.append("- changed rows : ${changedRows.size()}\n")
+sb.append("- equal rows:  : ${equalRows.size()}\n\n")
+
 sb.append("|line#|original|revised|\n");
 sb.append("|-----|--------|-------|\n");
 rows.eachWithIndex { DiffRow row, index ->
 	sb.append("|" + (index+1) + "|" + row.getOldLine() + "|" + row.getNewLine() + "|\n");
 }
+
 
 //println sb.toString()
 Path out = projectDir.resolve("out.md")
