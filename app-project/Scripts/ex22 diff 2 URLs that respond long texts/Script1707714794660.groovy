7902e065265f79bde39733ae7f52d04d31da506c
kazurayam <kazuaki.urayama@gmail.com>
2024-02-12 14:18:24 +0900

    add Test Cases/ex22 diff 2 URLs that respond long texts

---
+++ app-project/Scripts/ex22 diff 2 URLs that respond long texts/Script1707714794660.groovy
@@ -0,0 +1,9 @@
+/**
+ * ex22 diff 2 URLs that respond long texts
+ */
+
+CustomKeywords.'com.kazurayam.ks.TextsDiffer.diffURLs'(
+	"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css",    /* input as original */
+	"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css",   /* input as revised  */
+	"build/tmp/testOutput/ex22-output.md"           /* output */
+	)
